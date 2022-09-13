8496fd8fd4cef8e33034f845c1da980b000d9dc1
cah-logan-mccamish <logan.mccamish@cardinalhealth.com>
2022-09-13 18:09:41 +0000

    fix prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,4 @@ module "s3-bucket" {
   source  = "app.terraform.io/sept13-lmccam/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "logan-mccamish"
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
 }
